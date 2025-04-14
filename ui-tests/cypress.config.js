066ba648b853827b584eecf727be8cced1e81b33
Cristian Camilo Delgado <ccdelgado@outlook.es>
2025-04-14 10:03:31 -0500

    Fix: Use classic Cypress config to avoid require error

--- ui-tests/cypress.config.js
+++ ui-tests/cypress.config.js
@@ -1,18 +1,13 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
+module.exports = {
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports/.jsons",
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     baseUrl: "https://opensource-demo.orangehrmlive.com",
-    supportFile: false,
-    reporter: "mochawesome",
-    reporterOptions: {
-      reportDir: "cypress/reports/.jsons",
-      overwrite: false,
-      html: false,
-      json: true
-    },
-    setupNodeEvents(on, config) {
-      // Aquí puedes agregar hooks si los necesitas
-    }
+    supportFile: false
   }
-});
+};
