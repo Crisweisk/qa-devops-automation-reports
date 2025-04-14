bc81b87f58f0849b079d59f06ae484201c98884e
Cristian Camilo Delgado <ccdelgado@outlook.es>
2025-04-14 09:58:51 -0500

    Fix: Correct mochawesome reportDir path

--- ui-tests/cypress.config.js
+++ ui-tests/cypress.config.js
@@ -1,15 +1,18 @@
-module.exports = {
-  reporter: "mochawesome",
-  reporterOptions: {
-    reportDir: "ui-tests/cypress/reports/.jsons",
-    overwrite: false,
-    html: false,
-    json: true
-  },
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
     baseUrl: "https://opensource-demo.orangehrmlive.com",
+    supportFile: false,
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports/.jsons",
+      overwrite: false,
+      html: false,
+      json: true
+    },
     setupNodeEvents(on, config) {
-      // aquí se pueden agregar eventos si es necesario
+      // Aquí puedes agregar hooks si los necesitas
     }
   }
-};
+});
